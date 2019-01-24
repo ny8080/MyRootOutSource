d0bf16aa02cb76eb62c54df5696e997657c01800
hongsang <hongsang@2dfire.com>
2019-01-24 17:43:58 +0800

    ...

---
+++ HSTestView.h
@@ -0,0 +1,16 @@
+//
+//  HSTestView.h
+//  MyRootOutSource
+//
+//  Created by hongsang on 2019/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HSTestView : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HSTestView.m
@@ -0,0 +1,12 @@
+//
+//  HSTestView.m
+//  MyRootOutSource
+//
+//  Created by hongsang on 2019/1/24.
+//
+
+#import "HSTestView.h"
+
+@implementation HSTestView
+
+@end
